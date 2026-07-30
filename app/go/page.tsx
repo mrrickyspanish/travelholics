71624e3cf6fa52fb88fb449b995eae4ec1254609
mrrickyspanish <rj@creativeeyemultimedia.com>
2026-07-30 11:23:41 -0500

    Remove obsolete route-level logo override

--- app/go/page.tsx
+++ app/go/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import LaunchExperience from "@/components/launch/LaunchExperience";
 import { travelholicsLaunch } from "@/lib/travelholics-launch";
-import wordmarkStyles from "@/components/travelholics-wordmark.module.css";
 
 export const metadata: Metadata = {
   title: "Start Here | Travelholics",
@@ -11,9 +10,5 @@ export const metadata: Metadata = {
 };
 
 export default function GoPage() {
-  return (
-    <div className={wordmarkStyles.launchScope}>
-      <LaunchExperience config={travelholicsLaunch} />
-    </div>
-  );
+  return <LaunchExperience config={travelholicsLaunch} />;
 }
